7c5bcfd49fcdd126bc3b26700d833dc268e94709
levunam30101999 <levunam30101999@gmail.com>
2020-09-29 14:21:26 +0700

    Edit department

--- src/main/java/demo/TestMVC.java
+++ src/main/java/demo/TestMVC.java
@@ -8,14 +8,9 @@ import view.EmployeeView;
 
 public class TestMVC {
 	public static void main(String[] args) {
-//		 Lấy ra đối tượng Connection kết nối vào DB.
-//		EmployeeView v = new EmployeeView();
-//		EmployeeControl control = new EmployeeControl(v);
-//		control.run();
-//		control.exit();
-
-		DepartmentView v = new DepartmentView();
-		DepartmentControl control = new DepartmentControl(v);
+		//Lấy ra đối tượng Connection kết nối vào DB.
+		EmployeeView v = new EmployeeView();
+		EmployeeControl control = new EmployeeControl(v);
 		control.run();
 		control.exit();
 	}
